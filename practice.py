351926caad27e4b8f0392bf6e08abdbc20f05424
Dwight Da Silva (Luo Ji) <ddotdasilva@gmail.com>
2020-05-03 00:30:14 -0400

    fix error in practice.py and added a timestamp for print out of variabe 'a'. Also added image of the test ran for time.

--- practice.py
+++ practice.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+a = str (datetime.now())
 print('Welcome to the Git Practice')
-print('Program will stop here due to an error')
+print('Program will continue, error is corrected. ')
 print (a)
