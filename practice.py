dff32f009c19f542342c348de3e25c870a7a7783
Dwight Da Silva (Luo Ji) <ddotdasilva@gmail.com>
2020-05-05 11:54:33 -0400

    Incorperated the clock concept. Tested, debugged and tested. Code works as intented. this stage complete. Next is email feature.

--- practice.py
+++ practice.py
@@ -15,6 +15,24 @@ def LastTimestamp():                #fix to read last line only.
     with open('TimestampLog.txt', 'r') as f:
         return (list(f)[-1])        
 
+def CheckTime(lastTimeChecked):
+    time.sleep(1.0)
+    timeCheckNow = int(time.time() - t0)
+    compareTime = timeCheckNow - lastTimeChecked
+    print(f'Now = {timeCheckNow} | Last = {lastTimeChecked}') #for debugging
+    lastTimeChecked = timeCheckNow
+    if (compareTime % timeInterval == 0):
+        #go to Email()
+        print('Sent Email')  #for debugging
+        if (timeCheckNow >= timeLimit):
+            return 1
+        return 0
+    else:
+        if (timeCheckNow >= timeLimit):
+            return 1
+        return 0
+        
+"""    
 def  pro():
     time.sleep(2.5)
  #Only for Winndows right now.   
@@ -26,7 +44,10 @@ def clock():
     pro()
     pro()
     print (time.time() - t0, "seconds wall time")
-
+"""
+timeLimit= 22 # hard coded for now with be one of 3 option user selected
+Exit= 0
+timeInterval= 5
 a = str (LastTimestamp())
 print('Welcome to the Git Practice')
 print('Program will continue, error is corrected. ')
@@ -36,6 +57,13 @@ print('The new timestamp is at ' + b)
 NewTimeRecord(b)
 #DateTime format 
 print(f'Today is {datetime.now().strftime("%A %B %d, %Y")}')
-t0 = time.time()
-clock()
+t0 =  int(time.time())
+print(t0) #for debugging
+lastTimeChecked = int(time.time() - t0)
+print(f'last{lastTimeChecked}') #for debugging
+
+while (Exit == 0):
+    Exit = CheckTime(lastTimeChecked)
+    print(f'Exit is {Exit}') #for debugging
+#clock()
 print('End of Program')
